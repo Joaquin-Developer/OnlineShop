3b6e32161c05255bcc461eb0e53b53304373e903
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-01-19 17:30:45 -0300

    Configuración del proyecto

---
+++ InstallModules.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# librerías que requiere el proyecto (no incluidas en el repositorio de GutHub)
+# Ejecutar este script para instalarlas
+npm i nodemon
+npm i mysql
+npm i ejs
+npm i body-parser
+
