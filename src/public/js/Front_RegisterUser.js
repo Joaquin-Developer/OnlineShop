27e8177dd81d3c0a4f051ddfa3ba472fdccba45e
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-01-21 23:21:48 -0300

    avanzo en frontend de registro de usuarios

--- src/public/js/Front_RegisterUser.js
+++ src/public/js/Front_RegisterUser.js
@@ -2,6 +2,48 @@
  * Register user functions (login.html)
  */
 
-document.getElementById("btnRegisterUser").addEventListener("click", function() {
+const firstNameElem = document.querySelector("#first_name");
+const secondNameElem = document.querySelector("#second_name");
+const firstSurnameElem = document.querySelector("#first_surname");
+const secondSurnameElem = document.querySelector("#second_surname");
+const emailElem = document.querySelector("#email");
+const dirElem = document.querySelector("#dir");
+const verificationCodeElem = document.querySelector("#verification_code");
+const userNameElem = document.querySelector("#txtNewUsername");
+const passwElem = document.querySelector("#txtNewPassword");
+
+document.querySelector("#btnRegisterUser").addEventListener("click", function(event) {
+
+    event.preventDefault();
+
+    if (validString(firstNameElem.value) && validString(secondNameElem.value) 
+        && validString(firstSurnameElem.value) && validString(secondSurnameElem.value)
+        && emailElem.value && dirElem.value && verificationCodeElem.value && userNameElem.value && passwElem.value)
+    {
+        // valid/not-missing data:
+
+        
+        alert("ok");
+    } else {
+        // if missing data:
+        showErrorAlert("Error: Faltan datos y/o hay datos inválidos");    // function from Login.js
+    }
 
 });
+
+
+function validString(text) {
+    if (text === "") { return false; }
+    let array = text.replaceAll(" ", "").split("");
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "") {
+            continue;
+        }
+        if (! isNaN(array[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
