ae0c32a00e7ff033b7de6ec04f8cef5c136ca37f
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-02-01 01:02:35 -0300

    actualizo develop depend.

--- InstallModules.sh
+++ InstallModules.sh
@@ -3,7 +3,6 @@
 # librerías que requiere el proyecto (no incluidas en el repositorio de GutHub)
 # Ejecutar este script para instalarlas
 npm i express
-npm i nodemon
 npm i mysql
 npm i ejs
 npm i body-parser
@@ -11,3 +10,15 @@ npm i dotenv
 npm i nodemailer
 npm i nodemailer-smtp-transport
 npm i socket.io
+# Development dependences:
+read -p "Desea instalar dependencias? (si/no) " user
+
+if [ "$user" == "si" ] || [ "$user" == "SI" ]; then
+	echo "Instalando dependencias de desarrollo . . ."
+	npm i nodemon
+else
+	echo "No se instalarán las dependencias de desarrollo";
+	sleep 2;
+	clear
+fi
+
