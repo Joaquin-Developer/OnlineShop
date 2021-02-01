588f04a585ad02b165d88d3c68909989199aa433
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-01-31 22:24:54 -0300

    add pool mysql connection

--- src/controllers/ControllerDB.js
+++ src/controllers/ControllerDB.js
@@ -2,6 +2,37 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
+// const pool  = mysql.createPool({
+//     host: process.env.MYSQL_DB_HOST,
+//     user: process.env.MYSQL_DB_USER,
+//     password: process.env.MYSQL_DB_PASSW,
+//     database: process.env.DATABASE_NAME
+// });
+
+// pool.getConnection(function (error, connection) {
+//     if (error) {
+//         switch (error) {
+//             case "PROTOCOL_CONNECTION_LOST":
+//                 console.error("Database connection was closed");
+//                 break;
+//             case "ER_CON_COUNT_ERROR":  
+//                 console.error("Database has to many connections!");
+//                 break;
+//             case "ECONNREFUSED":
+//                 console.error("Database connection was refused");
+//                 break;
+//             default:
+//                 console.error("Database connection error");
+//                 break;
+//         }
+//     }
+//     if (connection) connection.release();
+//     console.log("MySQL Database Connected");
+//     return;
+// });
+
+// module.exports = pool;
+
 const connection = mysql.createConnection({
     host: process.env.MYSQL_DB_HOST,
     user: process.env.MYSQL_DB_USER,
